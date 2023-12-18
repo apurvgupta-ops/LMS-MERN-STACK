41f3ec038c5f9fac23b127b4b2dda5ba9afa3839
Apurv gupta <apurvgupta.124@gmail.com>
2023-12-18 16:48:01 +0530

    activate user

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -1,8 +1,9 @@
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
-import userModel from "../models/user.model";
+import userModel, { IUser } from "../models/user.model";
 import { NextFunction, Request, Response } from "express";
 import { createActivationToken } from "../utils/createActivationToken";
+import jwt, { Secret } from "jsonwebtoken";
 import sendMails from "../utils/sendMails";
 import ejs from "ejs";
 import path from "path";
@@ -12,6 +13,11 @@ interface IRegisterUser {
   email: string;
   password: string;
   avatar?: string;
+}
+
+interface IActivateUser {
+  activation_token: string;
+  activation_code: string;
 }
 
 export const userRegistration = CatchAsyncError(
@@ -56,6 +62,42 @@ export const userRegistration = CatchAsyncError(
       } catch (error: any) {
         return next(new ErrorHandler("Error in sending the mail", 400));
       }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+export const activateUser = CatchAsyncError(
+  async (res: Response, req: Request, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } = req.body as IActivateUser;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid Activation Code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+      const alreadyExistedMail = await userModel.find({ email });
+      if (alreadyExistedMail) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+
+      const user = await userModel.create({
+        name,
+        email,
+        password,
+      });
+
+      res.status(201).json({
+        success: true,
+        user,
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { userRegistration } from "../controller/user.controller";
+import { activateUser, userRegistration } from "../controller/user.controller";
 const userRoute = express.Router();
 
 userRoute.post("/registration", userRegistration);
+userRoute.post("/activate-user", activateUser);
 
 export default userRoute;
